362372d9e14058f6f161e42a1c0fae0a3e93ca16
Vladislav Semykin <vladislav_semykin01@mail.ru>
2022-11-22 15:12:54 +0300

    Added func of printing for tuples

--- lvtlib.cpp
+++ lvtlib.cpp
@@ -46,6 +46,43 @@ namespace lvt
                 std::cout << pair.first << " - " << pair.second << '\n';
             }
         }
+
+        namespace print_tuple
+        {
+            template <typename TupleType, int TupleSize>
+            class TuplePrintHelper
+            {
+            public:
+                TuplePrintHelper(const TupleType &t)
+                {
+                    TuplePrintHelper<TupleType, TupleSize - 1> tp{t};
+                    std::cout << std::get<TupleSize - 1>(t) << std::endl;
+                }
+            };
+
+            template <typename TupleType>
+            class TuplePrintHelper<TupleType, 0>
+            {
+            public:
+                TuplePrintHelper(const TupleType &) {}
+            };
+
+            template <typename T>
+            void printTuple(const T &t)
+            {
+                TuplePrintHelper<T, std::tuple_size<T>::value> tph{t};
+            }
+
+            // Example:
+            // std::tuple t{167, "Testing", false, 2.3, 53.46f};
+            // printTuple(t);
+            // Output:
+            // 167
+            // Testing
+            // 0
+            // 2.3
+            // 53.46
+        }
     }
 
     namespace string
